89f132c20e76949f36a64ddf2980ea5d92ea36cf
Wes Oldenbeuving <narnach@gmail.com>
2009-12-18 11:52:41 +0100

    Show which pattern matches a number

--- lib/number_recognizer.rb
+++ lib/number_recognizer.rb
@@ -1,11 +1,13 @@
 class NumberRecognizer
+  attr_accessor :type
+
   # Name -> [country code prefix match, digits after prefix]
   KNOWN_FORMATS = {
     'Dutch mobile' => [316, 8],
     'Dutch landline' => [/31[12345789]/, 8],
     'Belgian mobile' => [324, 8],
     'Suriname' => [597,7],
-    'Antilles' => [599,7]
+    'Dutch Antilles' => [599,7]
   }
   attr_accessor :number
 
@@ -14,7 +16,9 @@ class NumberRecognizer
   end
 
   def valid?
+    self.type = nil
     return false unless match = KNOWN_FORMATS.find {|name, pattern| number =~ /^0{0,2}#{pattern.first}\d{#{pattern.last},#{pattern.last}}$/}
-    match
+    self.type = match.first
+    true
   end
 end
--- spec/number_recognizer_spec.rb
+++ spec/number_recognizer_spec.rb
@@ -6,26 +6,31 @@ describe NumberRecognizer do
     it 'should recognize 0031612345678 as dutch mobile' do
       @nc = NumberRecognizer.new('0031612345678')
       @nc.should be_valid
+      @nc.type.should == 'Dutch mobile'
     end
 
     it 'should recognize 0031201234567 as dutch landline' do
       @nc = NumberRecognizer.new('0031201234567')
       @nc.should be_valid
+      @nc.type.should == 'Dutch landline'
     end
 
     it 'should recognize 0032412345678 as belgian mobile' do
       @nc = NumberRecognizer.new('0032412345678')
       @nc.should be_valid
+      @nc.type.should == 'Belgian mobile'
     end
 
     it 'should recognize 005971234567 as a Suriname number' do
       @nc = NumberRecognizer.new('005971234567')
       @nc.should be_valid
+      @nc.type.should == 'Suriname'
     end
 
     it 'should recognize 005991234567 as a Dutch Antilles number' do
       @nc = NumberRecognizer.new('005991234567')
       @nc.should be_valid
+      @nc.type.should == 'Dutch Antilles'
     end
   end
 end
