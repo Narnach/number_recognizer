590d829b91a0cf28308e89c7b8aa49845abfa315
Wes Oldenbeuving <narnach@gmail.com>
2009-12-18 11:41:00 +0100

    Use a lookup table instead of hand-crafted method to match a number

--- lib/number_recognizer.rb
+++ lib/number_recognizer.rb
@@ -1,4 +1,10 @@
 class NumberRecognizer
+  KNOWN_FORMATS = {
+    'Dutch mobile' => [316, 8],
+    'Belgian mobile' => [324, 8],
+    'Suriname' => [597,7],
+    'Antilles' => [599,7]
+  }
   attr_accessor :number
 
   def initialize(number)
@@ -6,22 +12,10 @@ class NumberRecognizer
   end
 
   def valid?
-    valid_dutch_mobile? or valid_belgian_mobile? or valid_suriname? or valid_antilles?
-  end
-
-  def valid_dutch_mobile?
-    number =~ /^00316\d{8,8}$/
-  end
-
-  def valid_belgian_mobile?
-    number =~ /^00324\d{8,8}$/
-  end
-
-  def valid_suriname?
-    number =~ /^00597\d{7,7}/
-  end
-
-  def valid_antilles?
-    number =~ /^00599\d{7,7}/
+    if match = KNOWN_FORMATS.find {|name, pattern| number =~ /^0{0,2}#{pattern.first}\d{#{pattern.last},#{pattern.last}}$/}
+      match
+    else
+      false
+    end
   end
 end
